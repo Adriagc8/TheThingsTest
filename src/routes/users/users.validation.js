a1cf89a8dc0621447eb0faa3f3c3e34960de0f42
adriagc8 <adrigonzalezcopado@gmail.com>
2024-05-09 11:12:07 +0200

    validation with customs messages

--- src/routes/users/users.validation.js
+++ src/routes/users/users.validation.js
@@ -1,58 +1,40 @@
-/**
- * Validation middleware for handling requests to the users endpoint.
- *
- * @module validation
- * @requires ../../utils/validate
- */
 const { validate, body, param, query } = require("../../utils/validate");
-/**
- * Validation rules for POST requests to the users endpoint.
- *
- * @typedef {Object} postValidation
- * @property {Object} body - The request body.
- * @property {String} body.name - The user's name. Must be a string, not empty, and no more than 64 characters long.
- * @property {String} body.surnames - The user's surnames. Must be a string, not empty, and no more than 264 characters long.
- */
 
-/**
- * Validation rules for GET requests to the users endpoint.
- *
- * @typedef {Object} getValidation
- * @property {Object} query - The request query parameters.
- * @property {String} [query.name] - The user's name. Must be a string and no more than 64 characters long.
- * @property {String} [query.surnames] - The user's surnames. Must be a string and no more than 264 characters long.
- */
-
-/**
- * The validation middleware for handling requests to the users endpoint.
- *
- * @type {Object}
- * @property {postValidation} post - The validation rules for POST requests.
- * @property {getValidation} get - The validation rules for GET requests.
- */
 const validation = {
   post:
     validate([
       body("name")
         .exists()
+        .withMessage('Name is required')
         .isString()
+        .withMessage('Name must be String')
         .notEmpty()
-        .isLength({ max: 64 }),
+        .withMessage('Name cannot be empty')
+        .isLength({ max: 64 })
+        .withMessage('Name max length 64'),
       body("surnames")
         .exists()
+        .withMessage('Surnames is required')
         .isString()
+        .withMessage('Surnames must be String')
         .notEmpty()
-        .isLength({ max: 264 }),
+        .withMessage('Surnames cannot be empty')
+        .isLength({ max: 264 })
+        .withMessage('Surnames max length 264'),
     ]) ,
   get: validate([
     query("name")
       .optional()
       .isString() 
-      .isLength({ max: 64 }),
+      .withMessage('Name must be String')
+      .isLength({ max: 64 })
+      .withMessage('Name max length 64'),
     query("surnames")
       .optional()
       .isString()
-      .isLength({ max: 264 }),
+      .withMessage('Surnames must be String')
+      .isLength({ max: 264 })
+      .withMessage('Surnames max length 264'),
   ]),
 };
 
